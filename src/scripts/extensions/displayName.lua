bd9facd42ae6cead776e9196ceeba526ad6007b3
denmou <denmou@foxmail.com>
2020-11-21 02:08:00 +0800

    添加晒肉价，烹饪锅时间计时，鹈鹕饥饿度展示

--- src/scripts/extensions/displayName.lua
+++ src/scripts/extensions/displayName.lua
@@ -11,6 +11,9 @@ EntityScript.GetDisplayName = function(self, ...)
         local pickable = self.components.pickable
         local hackable = self.components.hackable
         local fueled = self.components.fueled
+        local dryer = self.components.dryer
+        local stewer = self.components.stewer
+        local hunger = self.components.hunger
         if config_growthTime and config_growthTime.switch and (g_dlc_mode and config_growthTime.dlc[g_dlc_mode]) then
             if pickable then
                 if self.components.inspectable and self.components.inspectable:GetStatus(self) == "WITHERED" then
@@ -26,10 +29,23 @@ EntityScript.GetDisplayName = function(self, ...)
                 name = name .. "\n" .. g_obj_utils.timeFormat(math.ceil(currentTime))
             end
         end
-        if config_fuelTime and config_fuelTime.switch and (g_dlc_mode and config_fuelTime.dlc[g_dlc_mode]) and fueled then
-            local currentTime = math.ceil(fueled.currentfuel)
-            local maxTime = math.ceil(fueled.maxfuel)
-            name = name .. "\n" .. g_obj_utils.timeFormat(currentTime) .. "/" .. g_obj_utils.timeFormat(maxTime)
+        if config_fuelTime and config_fuelTime.switch and (g_dlc_mode and config_fuelTime.dlc[g_dlc_mode]) then
+            if fueled then
+                local currentTime = math.ceil(fueled.currentfuel)
+                local maxTime = math.ceil(fueled.maxfuel)
+                name = name .. "\n" .. g_obj_utils.timeFormat(currentTime) .. "/" .. g_obj_utils.timeFormat(maxTime)
+            end
+            if dryer and dryer:IsDrying() then
+                local currentTime = dryer:GetTimeToDry()
+                name = name .. "\n" .. g_obj_utils.timeFormat(math.ceil(currentTime))
+            end
+            if stewer and stewer:GetTimeToCook() > 0 then
+                local currentTime = stewer:GetTimeToCook()
+                name = name .. "\n" .. g_obj_utils.timeFormat(math.ceil(currentTime))
+            end
+            if hunger then 
+                name = name .. "\n" .. hunger:GetCurrent()
+            end
         end
     end
     return name
